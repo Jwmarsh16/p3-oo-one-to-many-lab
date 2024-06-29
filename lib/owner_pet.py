c3ebd125face824d74fd45ca00200ae8422a83f9
Jonathan Marshall <jwmarsh16@gmail.com>
2024-06-29 12:52:52 -0500

    lab complete

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,33 @@
 class Pet:
+    
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    def __init__(self, name, pet_type, owner = None):
+        self.name = name
+        if pet_type not in Pet.PET_TYPES:
+            raise ValueError(f"Invalid pet type: {pet_type}. Must be one of {Pet.PET_TYPES}.")
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+        
+
     pass
 
 class Owner:
+
+    def __init__ (self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    
+    def add_pet(self, pet):
+        if isinstance(pet, Pet):
+            pet.owner = self
+        else:
+            raise ValueError(f"{pet} is not an instance of Pet.")
+
+    def get_sorted_pets(self):
+        return sorted(self.pets(), key = lambda pet: pet.name)
+            
     pass
